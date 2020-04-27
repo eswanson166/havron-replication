32c13c6fd60862b895d21252b6646501e5ae0d12
Elizabeth Swanson <elizswan@stanford.edu>
2020-04-27 02:31:08 -0700

    added eyetracking template

---
+++ eyetracking_template_245b/exp/js/exp_files/img_fnames.js
@@ -0,0 +1,12 @@
+var img_fnames = {
+"with dark hair": ["t_h2_b1", "v_h1_b2"],
+"with blonde hair": [ "t_h1_b6", "v_h2_b7" ],
+"wearing pants" : ["t_h3_b4", "v_h1_b5"],
+"wearing a dress" : ["t_h1_b3", "v_h7_b5"],
+"wearing black" : ["v_h3_b7" ,"t_h2_b4"],
+"wearing white" : ["v_h2_b5", "t_h5_b5"],
+"who looks tougher" : ["v_h1_b4","t_h4_b2"],
+"who looks more feminine" : ["v_h2_b1", "t_h4_b6"],
+"on the left" : ["v_h5_b1blue", "v_h3_b3"],
+"on the right" : ["t_h2_b5", "t_h1_b7"]
+}
